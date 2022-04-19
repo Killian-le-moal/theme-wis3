611836b23d46f892e9c8c5549df5e53fc274f454
Rémy <remy.guillet44@gmail.com>
2022-04-19 15:46:30 +0200

    update : contact

--- page-templates/contact.php
+++ page-templates/contact.php
@@ -6,7 +6,9 @@ get_header(); ?>
 
 <section class="main py-5 ">
     <div class=" container">
-
+    <div class="card card-header bg-light text-primary mb-4 text-center">
+        <h2>Nous contacter</h2>
+    </div>
         <div class="card card-body form-wrapper mx-auto" style="max-width: 800px;">
         <?php echo do_shortcode('[contact-form-7 id="71"]');  ?>
         </div>
