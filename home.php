8f6871088218eb82d9d0f1e7876dc769e299e45f
Rémy <remy.guillet44@gmail.com>
2022-04-19 16:50:41 +0200

    update : home

--- home.php
+++ home.php
@@ -1,21 +1,25 @@
 <?php get_header(); ?>
     <main id="main" class="site-main" role="main">
-        <?php $query = new WP_Query(array(
-            'post_type' => 'post',
-            'posts_per_page' => 10
-        )); ?>
-
-        <?php if ($query->have_posts()): ?>
-            <div class="row g-4">
-                <?php while ($query->have_posts()) : $query->the_post(); ?>
-                    <div class="col-md-6 col-xl-4">
-                        <?php get_template_part('template-parts/post'); ?>
-                    </div>
-                <?php endwhile;
-                wp_reset_postdata(); ?>
+        <section class="container">
+            <div class="text-center mb-4">
+                <h1>Nos dernières actus...</h1>
             </div>
-            <?php get_template_part('template-parts/pagination'); ?>
-        <?php endif; ?>
+            <?php $query = new WP_Query(array(
+                'post_type' => 'post',
+                'posts_per_page' => 10
+            )); ?>
 
+            <?php if ($query->have_posts()): ?>
+                <div class="row g-4">
+                    <?php while ($query->have_posts()) : $query->the_post(); ?>
+                        <div class="col-md-6 col-xl-4">
+                            <?php get_template_part('template-parts/post'); ?>
+                        </div>
+                    <?php endwhile;
+                    wp_reset_postdata(); ?>
+                </div>
+                <?php get_template_part('template-parts/pagination'); ?>
+            <?php endif; ?>
+        </section>
     </main><!-- #main -->
 <?php get_footer();
